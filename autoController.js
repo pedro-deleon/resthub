4e87712307cc838e9521c09fe6fb95eccfcebe41
Pedro de León <38962557+Turtugilla@users.noreply.github.com>
2020-03-24 14:16:14 -0600

    initial commit

---
+++ autoController.js
@@ -0,0 +1,88 @@
+// autoController.js
+
+// Import auto model
+Auto = require('./autoModel');
+exports.index = function (req, res) {
+    Auto.get(function (err, autos) {
+        if (err) {
+            res.json({
+                status: "error",
+                message: err
+            });
+        }
+        res.json({
+            status: "success",
+            message: "Autos retrieved successfully",
+            data: autos
+        });
+    }, 30);
+};
+
+// Handle create auto actions
+exports.new = function (req, res) {
+    var auto = new Auto();
+    auto.id = req.body.id ? req.body.id : auto.id;
+    auto.marca = req.body.marca;
+    auto.submarca = req.body.submarca;
+    auto.descripcion = req.body.descripcion;
+    auto.modelos = req.body.modelos;
+
+    auto.save(function (err) {
+        res.json({ message: 'New auto created!', data: auto });
+    });
+};
+
+
+// Handle view auto info
+exports.view = function (req, res) {
+    Auto.findById(req.params.auto_id, function (err, auto) {
+        if (err)
+            res.send(err);
+
+        res.json({
+            message: 'Auto details loading...',
+            data: auto
+        });
+    });
+};
+
+
+// Handle update auto info
+exports.update = function (req, res) {
+    Auto.findById(req.params.auto_id, function (err, auto) {
+        if (err)
+            res.send(err);
+
+        auto.name = req.body.name ? req.body.name : auto.name;
+        auto.gender = req.body.gender;
+        auto.email = req.body.email;
+        auto.phone = req.body.phone;
+
+        // save the auto and check for errors
+        auto.save(function (err) {
+            if (err)
+                res.json(err);
+            res.json({
+                message: 'Auto Info update',
+                data: auto
+            });
+        });
+    });
+};
+
+//Handle delete auto
+exports.delete = function (req, res) {
+    Auto.remove({
+        _id: req.params.auto_id
+    }, function (err, auto) {
+        if (err)
+            res.send(err);
+
+        res.json({
+            status: "success",
+            message: 'Auto deleted'
+        });
+    });
+}
+
+
---
+++ autoModel.js
@@ -0,0 +1,26 @@
+var mongoose = require('mongoose');
+
+// Setup schema
+
+var autoSchema = mongoose.Schema({
+    id: {
+        type: Number,
+        required: true
+    }, marca: {
+        type: String,
+        required: true
+    },
+    submarca: {
+        type: String, required: true
+    },
+    modelos: { type: Number, required: true },
+    descripcion: { type: String, required: true }
+});
+
+// Export Auto Model
+
+var Auto = module.exports = mongoose.model('auto', autoSchema)
+
+module.exports.get = function (callback, limit) {
+    Auto.find(callback).limit(limit);
+}
