18fc3ef9700172109f849da25bca8a13eae4ad72
Pedro de León <38962557+Turtugilla@users.noreply.github.com>
2020-03-25 00:22:42 -0600

    Update autoController.js

--- autoController.js
+++ autoController.js
@@ -32,7 +32,7 @@ exports.limit = function (req, res) {
             message: "Autos retrieved successfully",
             data: autos
         });
-    }, req.params.limitNumber);
+    }, +req.params.limitNumber);
 };
 
 
