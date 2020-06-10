374dd9b42842a66292ee12b6c30446fe499ffffc
Pedro de León <38962557+Turtugilla@users.noreply.github.com>
2020-06-09 19:02:17 -0500

    changes to cors

--- index.js
+++ index.js
@@ -7,6 +7,8 @@ let mongoose = require('mongoose');
 // Initialise the app
 let app = express();
 
+const cors = require('cors');
+
 // Import routes
 let apiRoutes = require("./api-routes");
 // Configure bodyparser to handle post requests
@@ -33,18 +35,7 @@ if (port == null || port == "") {
     port = 8080;
 }
 
-var allowCrossDomain = function (req, res, next) {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
-
-    // intercept OPTIONS method
-    if ('OPTIONS' == req.method) {
-        res.status(200);
-    } else {
-        next();
-    }
-};
+app.use(cors());
 
 app.use(allowCrossDomain);
 
